d7f5cd9c719da0c6a61d946bccbc8d2a8feca3dc
y.to <suteakaga117@gmail.com>
2019-07-24 16:30:23 +0000

    CRUD messages

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,47 @@
+class TasksController < ApplicationController
+    def index
+        @tasks = Task.all
+    end
+    def create
+        @task = Task.new(task_params)
+        if @task.save
+            flash[:success] = 'タスクが追加されました'
+            redirect_to @task
+        else
+            flash.now[:danger] = 'タスクの追加に失敗しました'
+            render :new
+        end
+    end
+    def new
+        @task = Task.new
+    end
+    def edit
+        @task = Task.find(params[:id])
+    end
+    def show
+        @task = Task.find(params[:id])
+    end
+    def update
+        @task = Task.find(params[:id])
+    
+        if @task.update(task_params)
+          flash[:success] = 'タスク修正完了'
+          redirect_to @task
+        else
+          flash.now[:danger] = 'タスクの修正に失敗しました'
+          render :edit
+        end
+    end
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        
+        flash[:success] = 'タスクを削除しました'
+        redirect_to tasks_url
+    end
+    
+    private
+    def task_params
+    params.require(:task).permit(:content)
+    end
+end
