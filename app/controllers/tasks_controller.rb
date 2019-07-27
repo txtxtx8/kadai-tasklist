b66387762da3a4d42f6bfea40b6fcc5729d351b9
y.to <suteakaga117@gmail.com>
2019-07-27 05:24:32 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
     def index
-        @tasks = Task.all
+        @tasks = Task.all.page(params[:page]).per(5)
     end
     def create
         @task = Task.new(task_params)
